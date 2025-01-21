f5248698a80f32ddd2d1f05815ba4038fefc4a48
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2025-01-21 20:46:40 -0300

    Funcionalidad para el buscador de productos

--- src/components/productsPanel/ProductsPanel.jsx
+++ src/components/productsPanel/ProductsPanel.jsx
@@ -7,16 +7,20 @@ export default function ProductsPanel() {
   const products = useSelector((state) => state.products.productsCopy);
 
   return (
-        <div className={styles.products_container}>
-          <ul className={styles.products_ul}>
-            {products.map((product) => {
-              return (
-                <li className={styles.products_li} key={product.id}>
-                  <CardProduct product={product} />
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+    <div className={styles.products_container}>
+      {products.length === 0 ? (
+        <p className={styles.products_search_p}>
+          No hay productos para esta búsqueda...
+        </p>
+      ) : (
+        <ul className={styles.products_ul}>
+          {products.map((product) => (
+            <li className={styles.products_li} key={product.id}>
+              <CardProduct product={product} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
